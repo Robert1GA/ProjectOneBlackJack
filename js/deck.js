be6888fe26bc984a52135642d3d0bf2839a942fb
Robert1GA <roberts.2015@yahoo.co.uk>
2016-12-10 12:58:30 -0800

    attempting to make array of one deck of cards

--- js/deck.js
+++ js/deck.js
@@ -1,8 +1,68 @@
-function carsFullDeck (card, suit, value, img) {
+class cards {
+  constructor (card, suit, value, img) {
   this.card = card;
   this.suit = suit;
   this.value = value;
   this.img = img;
+  }
 }
 
-var ace1 = new card ("ace", "club", 11, "#");
+var allCards = [];
+
+  allCards.push(
+    card0 = new cards ("ace", "club", 11, "#"),
+    card1 = new cards ("ace", "diamonds", 11, "#"),
+    card2 = new cards ("ace", "hearts", 11, "#"),
+    card3 = new cards ("ace", "spades", 11, "#"),
+
+    card4 = new cards ("two", "club", 2, "#"),
+    card5 = new cards ("two", "diamonds", 2, "#"),
+    card6 = new cards ("two", "hearts", 2, "#"),
+    card7 = new cards ("two", "spades", 2, "#"),
+
+    card8 = new cards ("three", "club", 3, "#"),
+    card9 = new cards ("three", "diamonds", 3, "#"),
+    card10 = new cards ("three", "hearts", 3, "#"),
+    card11 = new cards ("three", "spades", 3, "#"),
+
+    card12 = new cards ("four", "club", 4, "#"),
+    card13 = new cards ("four", "diamonds", 4, "#"),
+    card14 = new cards ("four", "hearts", 4, "#"),
+    card15 = new cards ("four", "spades", 4, "#"),
+
+    card16 = new cards ("five", "club", 5, "#"),
+    card17 = new cards ("five", "diamonds", 5, "#"),
+    card18 = new cards ("five", "hearts", 5, "#"),
+    card19 = new cards ("five", "spades", 5, "#"),
+
+    card20 = new cards ("six", "club", 6, "#"),
+    card21 = new cards ("six", "diamonds", 6, "#"),
+    card22 = new cards ("six", "hearts", 6, "#"),
+    card23 = new cards ("six", "spades", 6, "#"),
+
+    card24 = new cards ("seven", "club", 7, "#"),
+    card25 = new cards ("seven", "diamonds", 7, "#"),
+    card26 = new cards ("seven", "hearts", 7, "#"),
+    card27 = new cards ("seven", "spades", 7, "#"),
+
+    card28 = new cards ("eight", "club", 8, "#"),
+    card29 = new cards ("eight", "diamonds", 8, "#"),
+    card30 = new cards ("eight", "hearts", 8, "#"),
+    card31 = new cards ("eight", "spades", 8, "#"),
+
+    card32 = new cards ("nine", "club", 9, "#"),
+    card33 = new cards ("nine", "diamonds", 9, "#"),
+    card34 = new cards ("nine", "hearts", 9, "#"),
+    card35 = new cards ("nine", "spades", 9, "#"),
+
+    card36 = new cards ("ten", "club", 10, "#"),
+    card37 = new cards ("ten", "diamonds", 10, "#"),
+    card38 = new cards ("ten", "hearts", 10, "#"),
+    card39 = new cards ("ten", "spades", 10, "#")
+  );
+
+
+console.log(allCards[0]);
+console.log(allCards[1]);
+console.log(allCards[39]);
+console.log(allCards.length);
---
+++ js/script.js
@@ -0,0 +1,17 @@
+$("document").ready(function() {
+  console.log("ready!");
+
+
+
+  // var pullCard = "card" + Math.round(Math.random() * cards.length);
+  // console.log(dealt);
+
+  // function dealCard() {
+  //   var dealt = Math.round(Math.random() * cards.length);
+  //   console.log(dealt);
+  // }
+
+
+
+
+});

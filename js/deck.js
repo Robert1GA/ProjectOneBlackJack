e5c5ba3a072fe481b18d76c3317f2f7a2c1c905f
Robert1GA <roberts.2015@yahoo.co.uk>
2016-12-09 16:35:17 -0800

    just starting the outline of the game; creating structure of cards

---
+++ js/deck.js
@@ -0,0 +1,8 @@
+function carsFullDeck (card, suit, value, img) {
+  this.card = card;
+  this.suit = suit;
+  this.value = value;
+  this.img = img;
+}
+
+var ace1 = new card ("ace", "club", 11, "#");

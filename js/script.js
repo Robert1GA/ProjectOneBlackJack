6df3005704acca0af044e7fd77191d870183a91b
Robert1GA <roberts.2015@yahoo.co.uk>
2016-12-12 09:47:16 -0800

    deal button created on load; pressing initiates game. buttons added for hit,stay; logic to follow

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
 $("document").ready(function() {
   console.log("ready!");
 
+  var deck = [];
+  deck = ALLCARDS;
 
-  console.log(ALLCARDS[0]);
-  console.log(ALLCARDS.length);
+  $("#hit").toggle();
+  $("#stay").toggle();
+  $("#double").toggle();
+
+  console.log("deck card0:", deck[0]);
+  console.log("deck length:", deck.length);
+
+  // event listener on the button that
+  $("#dealCards").click(function(e){
+    e.preventDefault();
+    startingCards();
+    $("#dealCards").toggle();
+    $("#hit").toggle();
+    $("#stay").toggle();
+    $("#double").toggle();
+  });
+
+  $("#hit").click(function(e){
+    e.preventDefault();
+  });
+
+  // event listener on the button that
+  $("#stay").click(function(e){
+    e.preventDefault();
+  });
+
+  // event listener on double-down button
+  $("#double").click(function(e){
+    e.preventDefault();
+  });
+
+
 
   function generateRandomCard() {
-    var rand = Math.round(Math.random() * (ALLCARDS.length-1));
-    var aCard = ALLCARDS[rand];
+    var rand = Math.round(Math.random() * (deck.length-1));
+    var aCard = deck[rand];
     console.log(rand);
     console.log(aCard);
-    ALLCARDS.splice(rand,1);
+    deck.splice(rand,1);
     return aCard;
   }
 
@@ -25,10 +57,11 @@ $("document").ready(function() {
     console.log("player:",playerCards);
     console.log("dealers value",dealerCards[0].value+dealerCards[1].value);
     console.log("players value",playerCards[0].value+playerCards[1].value);
+    console.log("new deck length:", deck.length);
     displayCards(dealerCards,playerCards);
   }
 
-  startingCards();
+
 
   function displayCards(dealerCards,playerCards) {
     $("#dealercard0").attr("src", dealerCards[0].img);
